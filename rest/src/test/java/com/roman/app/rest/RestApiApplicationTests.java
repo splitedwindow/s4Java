e57ba14c0275beb27cf0db5dd3a7af3ac4c7a39c
Roman <rom@kozova.net>
2024-05-15 12:11:01 +0300

    test worked

--- rest/src/test/java/com/roman/app/rest/RestApiApplicationTests.java
+++ rest/src/test/java/com/roman/app/rest/RestApiApplicationTests.java
@@ -1,28 +1,35 @@
-//package com.roman.app.rest;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.TestPropertySource;
-//
-//@SpringBootTest(
-//		webEnvironment = SpringBootTest.WebEnvironment.MOCK,
-//		classes = RestApiApplication.class)
-//@AutoConfigureMockMvc
-//@TestPropertySource(
-//		locations = "classpath:application-integrationtest.properties")
-//class RestApiApplicationTests {
-//
-//	@Test
-//	void getPage_thenReturnWelcome()
-//		throws Exception {
-//
-//
-//			mvc.perform(get("/")
-//							.contentType(MediaType.APPLICATION_JSON))
-//					.andExpect(status().isOk())
-//					.andExpect(content()
-//							.contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-//					.andExpect(jsonPath("$[0].name", is("bob")));
-//		}
-//}
+package com.roman.app.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+        classes = RestApiApplication.class)
+@AutoConfigureMockMvc
+@TestPropertySource(
+        locations = "classpath:application-integrationtest.properties")
+class RestApiApplicationTests {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void getPage_thenReturnWelcome() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Welcome"));
+    }
+}
