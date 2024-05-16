03816e737f36fdae18cbcc2b2eda4f396dc3b94a
Roman <rom@kozova.net>
2024-05-16 14:18:33 +0300

    stuck

--- rest/src/main/java/com/roman/app/rest/Models/customUserDetailsService.java
+++ rest/src/main/java/com/roman/app/rest/Models/customUserDetailsService.java
@@ -1,5 +1,6 @@
 package com.roman.app.rest.Models;
 
+import com.roman.app.rest.Repo.rolesRepo;
 import com.roman.app.rest.Repo.userRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -7,17 +8,31 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class customUserDetailsService implements UserDetailsService {
+
     @Autowired
-    userRepo customerR;
+    private userRepo customerR;
+
+    @Autowired
+    private rolesRepo rolesR;
+
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         user User = customerR.findByEmail(email);
 
-        if(email == null) {
-            throw new UsernameNotFoundException("Could not find user with email");
+        if (User == null) {
+            throw new UsernameNotFoundException("User not found with email: " + email);
         }
-        return new customUserDetails(User);
+
+        Optional<roles> roleOpt = rolesR.findByRoleId(User.getRoleId());
+        roles role = roleOpt.orElseThrow(() -> new UsernameNotFoundException("Role not found for user with email: " + email));
+
+        return new customUserDetails(User, Collections.singletonList(role));
     }
 }
+
